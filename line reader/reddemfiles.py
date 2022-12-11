25f5486b303b1cf7421c6b79a4c67d3aa6d084df
Narwhalssss <114067296+Narwhalssss@users.noreply.github.com>
2022-12-11 08:55:00 +0700

    Add files via upload

---
+++ line reader/reddemfiles.py
@@ -0,0 +1,15 @@
+fileread = open('me.txt')
+
+reademlist = [fileread.read().splitlines()]
+
+
+
+removen = []
+#for i in range(len(reademlist)):
+#    if (reademlist[i][-2]+reademlist[i][-1]) == "\n":
+#        reademlist[i] = reademlist[i].replace('\n',' ')
+    
+
+
+print(reademlist)
+    
